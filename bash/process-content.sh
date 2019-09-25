b1ddc22397f2a0035ffdcb7be6d1d76015782510
Roberto Srebot <roberto@mural.co>
2019-09-25 10:02:01 -0300

    Get thumbanails to copy

--- bash/process-content.sh
+++ bash/process-content.sh
@@ -2,3 +2,13 @@ jq 'select(.widgets!=null) | .widgets[] | select(.properties!=null)  | .properti
 cat assets.txt | sed -e 's#/.*##' | sort -u > ws1.txt
 diff --new-line-format="" --unchanged-line-format="" <(sort ws1.txt) <(sort ws.txt) > diff.txt
 while read line; do grep -wF "$line" assets.txt; done < diff.txt | tee extra-assets.txt
+
+#Thumnails
+grep -Po  '"https://mural.azureedge.net/thumbnails/.*?"' muralcontent.json | sed 's/"//g' | sed -e 's/https:\/\/mural.azureedge.net\/thumbnails\///g' | sed -e 's/?v.*//g' > thumbs.txt
+cat thumbs.txt | sed -e 's#/.*##' | sort -u > ws2.txt
+
+diff --new-line-format="" --unchanged-line-format="" <(sort ws2.txt) <(sort ws.txt) > diff2.txt
+while read line; do grep -wF "$line" thumbs.txt; done <  diff2.txt | tee thumbnails.txt
+
+# Remove temp files
+# rm ws1.txt ws2.txt diff.txt diff2.txt
