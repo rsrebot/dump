33f54797dc70832e8610a012ec7252ba6c08afd7
Roberto Srebot <roberto@mural.co>
2019-09-25 08:34:59 -0300

    fixed restore templates

--- bash/restore-templates.sh
+++ bash/restore-templates.sh
@@ -8,6 +8,6 @@ read -p "Company to dump: " company
 
 input=./$company/global-templates/$source_db
 
-mongorestore --host $source_host -u $source_user -p $source_pass -vvvvv --authenticationDatabase $source_auth_db --sslAllowInvalidCertificates --ssl --numInsertionWorkersPerCollection $inserters $input
+mongorestore --host $dest_host -u $dest_user -p $dest_pass -vvvvv --authenticationDatabase $dest_auth_db --sslAllowInvalidCertificates --ssl --db $dest_db --numInsertionWorkersPerCollection $inserters $input
 
 echo DONE
