8cc570a53bdd0d23b77698829eeaf209e2030f00
Roberto Srebot <roberto@mural.co>
2019-10-01 09:22:57 -0300

    Remove cat

--- bash/process-content.sh
+++ bash/process-content.sh
@@ -3,7 +3,7 @@ cat assets.txt | sed -e 's#/.*##' | sort -u > ws1.txt
 diff --new-line-format="" --unchanged-line-format="" <(sort ws1.txt) <(sort ws.txt) > diff.txt
 while read line; do grep -wF "$line" assetsx.txt; done < diff.txt | tee extra-assets.txt
 
-#Thumnails
+#Thumbnails
 grep -Po  '"https://mural.azureedge.net/thumbnails/.*?"' muralcontent.json | sed 's/"//g' | sed -e 's/https:\/\/mural.azureedge.net\/thumbnails\///g' | sed -e 's/?v.*//g' > thumbs.txt
 cat thumbs.txt | sed -e 's#/.*##' | sort -u > ws2.txt
 
@@ -12,7 +12,6 @@ while read line; do grep -wF "$line" thumbs.txt; done <  diff2.txt | tee thumbna
 
 # Avatars
 members="$(cat tempws.json | jq -r '.membersIndex[]' | sort -u | tr '\n' '|' | sed 's/|$//')"
-echo $members
 cat profiles.json | grep -v '"type":"organization"' | grep -v '$members' | sed -ne 's/.*"avatar":"\([^"?]*\).*/\1/p' | sed -e 's/https:\/\/murally.blob.core.windows.net:443\/uploads\///g' | grep -v 'https:' > avatars.txt
 
 # Remove temp files
